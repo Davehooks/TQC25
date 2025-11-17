203e48085e7bd87c948a04778180023a47d44325
Davehooks <150075150+Davehooks@users.noreply.github.com>
2025-11-16 22:37:17 -0300

    0.08.02_Reconfigurando_2

--- TCQ-25/Assets/Core/Scripts/Sound/CallSFX.cs
+++ TCQ-25/Assets/Core/Scripts/Sound/CallSFX.cs
@@ -8,18 +8,37 @@ public class CallSFX : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        AudioSource = this.GetComponent<AudioSource>();
+        AudioSource = this.gameObject.GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
     public void CallSfx(int i)
     {
-        AudioSource.clip = AudioClip[i];
-        AudioSource.Play();
+        if (AudioClip != null)
+        {
+            AudioSource.clip = AudioClip[i];
+            if (!AudioSource.isPlaying)
+            {
+                AudioSource.Play();
+            }
+        }
     }
     public void CallSfxAnimation(int i)
     {
-        AudioSource.clip = AudioClip[i];
-        AudioSource.Play();
+        if (AudioClip != null)
+        {
+            AudioSource.clip = AudioClip[i];
+            if (!AudioSource.isPlaying)
+            {
+                AudioSource.Play();
+            }
+        }
+    }
+    public void StopSFX()
+    {
+        if (AudioSource.isPlaying)
+        {
+            AudioSource.Stop();
+        }
     }
 }
